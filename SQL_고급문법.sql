831ce7c9fb13f13d0e4d9e932842e1cfd025f07c
uk1996 <csuk1947@gmail.com>
2022-08-21 21:48:49 +0900

    update SQL_고급문법.sql

--- SQL_고급문법.sql
+++ SQL_고급문법.sql
@@ -37,3 +37,21 @@ CREATE TABLE movie (
 	-- DATE: 날짜만 저장(YYYY-mm-dd)
     -- TIME: 시간만 저장(HH:MM:SS)
     -- DATETIME: 날짜와 시간 모두 저장(YYYY-mm-dd HH:MM:SS)
+    
+
+-- 변수의 사용
+USE market_db;
+
+SET @myVar1 = 5;
+SET @myVar2 = 4.25;
+SELECT @myVar1;
+SELECT @myVar1 + @myVar2;
+
+SET @txt = '가수 이름 ==> ';
+SET @height = 166;
+SELECT @txt, mem_name FROM member WHERE height > @height;
+
+-- PREPARE, EXECUTE(LIMIT에서는 변수를 사용할 수 없음)
+SET @count = 3;
+PREPARE mySQL FROM 'SELECT mem_name, height FROM member ORDER BY height LIMIT ?';
+EXECUTE mySQL USING @count;
